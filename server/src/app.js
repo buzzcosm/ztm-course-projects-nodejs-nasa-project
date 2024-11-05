903d33fadf4a280675b77c40dd6f054f6a48feee
Evgueni Kounik <kounik.ch@gmail.com>
2024-11-05 14:29:08 +0100

    Add new controller function for post and improve routes call

--- server/src/app.js
+++ server/src/app.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 // backend routing
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+// frontend routing
 if (process.env.NODE_ENV === 'production') {
-  // frontend routing
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"));
   });
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,13 +1,16 @@
 // launches.model.js
 const launches = new Map();
 
+// state
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
   destination: 'Kepler-442 b',
-  customers: ['SpaceX', 'NASA'],
+  customers: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 };
@@ -18,6 +21,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber, 
+    Object.assign(launch, {
+      upcoming: true,
+      success: true,
+      customers: ['SpaceX', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 }
