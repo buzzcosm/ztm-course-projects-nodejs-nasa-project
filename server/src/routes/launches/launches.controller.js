c9ead4d778a5a64f73d5bb9c19f2b5b8f76f87ba
Evgueni Kounik <kounik.ch@gmail.com>
2024-11-05 15:05:53 +0100

    Add request body validation on backend

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -9,8 +9,32 @@ function httpGetAllLaunches(req, res) {
 }
 
 function httpAddNewLaunch(req, res) {
-  let launch = req.body;
+  const launch = req.body;
+  // validation
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({ // client error - 400 Bad Request
+      error: 'Missing required launch property',
+    });
+  }
   launch.launchDate = new Date(launch.launchDate);
+  // other validation approaches for the date
+  // isNaN() -> returns true if the value is not a number
+  // isNaN(launch.launchDate) -> returns true if the value is not a number
+  // date.valueOf() -> returns the number of milliseconds since January 1, 1970
+  // new Date().valueOf() -> returns the number of milliseconds since January 1, 1970
+  // 
+  // if (launch.launchDate.toString() === 'Invalid Date') {
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({ // client error - 400 Bad Request
+      error: 'Invalid launch date',
+    });
+  }
+
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
