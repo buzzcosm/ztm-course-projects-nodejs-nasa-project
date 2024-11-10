3effa0cffdbb67a4a238c5e9c5d778e74db31269
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-10 21:36:01 +0100

    Install and add mongoose to the server and setup env

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = require('./app');
 
@@ -6,9 +9,24 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+// MongoDB - config
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
+const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
+const MONGO_URL = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@nasacluster.gwf5l.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=NASACluster`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   
   server.listen(PORT, () => {
