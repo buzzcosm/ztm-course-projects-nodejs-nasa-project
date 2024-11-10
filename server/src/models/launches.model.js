89aff5e303b96bedfffa15f3fba317044398c0bb
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-11 00:07:08 +0100

    Add all launches handler

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,6 @@
 // launches.model.js
-// const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -17,14 +18,33 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDatabase.find({}, {
+    '_id': 0,
+    '__v': 0
+  });
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error(`No matching planet found for ${launch.target}`);
+  }
+
+  await launchesDatabase.updateOne({
+    flightNumber: launch.flightNumber,
+  }, launch, {
+    upsert: true
+  });
 }
 
 function addNewLaunch(launch) {
