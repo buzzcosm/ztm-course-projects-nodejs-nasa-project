d3924af0dd6ce240000a36863a65b8b7d9b9633b
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-12 22:32:06 +0100

    Add mongo connection service

--- server/src/server.js
+++ server/src/server.js
@@ -1,32 +1,14 @@
-const http = require('http');
-const mongoose = require('mongoose');
-
 require('dotenv').config();
-
+const http = require('http');
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
-
-// MongoDB - config
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
-const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
-const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
-const MONGO_URL = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@nasacluster.gwf5l.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=NASACluster`;
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
   
   server.listen(PORT, () => {
