bfa50d639b95a17da339d1d6cc9c0da7131675a7
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-12 18:38:03 +0100

    Finished launches abort functionality

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,8 +4,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
-
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
@@ -19,8 +17,10 @@ const launch = {
 
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId
+  });
 }
 
 async function getLatestFlightNumber() {
@@ -71,11 +71,15 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId
+  }, {
+    upcoming: false,
+    success: false,
+  });
+
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
