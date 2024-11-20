595c5a912c69e458b83f4e1a0fb25d20dcd4f6f0
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-20 01:50:47 +0100

    Change target in data-model of launch to load spacex lauch data into mongodb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -44,6 +44,11 @@ async function populateLaunches() {
     },
   });
 
+  if (response.status !== 200) {
+    console.log('Problem downloading launch data');
+    throw new Error('Launch data download failed');
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc['payloads'];
@@ -61,9 +66,7 @@ async function populateLaunches() {
       customers,
     };
     
-    console.log(`Launch: ${launch.flightNumber} ${launch.mission}`);
-
-    // TODO: populate launches collection...
+    await saveLaunch(launch);
   }
 }
 
@@ -110,14 +113,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error(`No matching planet found for ${launch.target}`);
-  }
-
   await launchesDatabase.findOneAndUpdate({
     flightNumber: launch.flightNumber,
   }, launch, {
@@ -126,6 +121,14 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error(`No matching planet found for ${launch.target}`);
+  }
+
   const newFlightNumber = await getLatestFlightNumber() + 1;
 
   const newLaunch = Object.assign(launch, {
