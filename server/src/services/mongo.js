3f6514a2ae1226c03b57f8e59a40c58debe3fe84
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-24 12:57:06 +0100

    Improve code set require('dotenv').config() once in server.js

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,11 +1,6 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
-// MongoDB - config
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
-const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
-const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
-const MONGO_URL = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@nasacluster.gwf5l.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=NASACluster`;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
